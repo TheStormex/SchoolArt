6d5fd58cd11124e2e59270c188e7e96d33d57efc
TheStormex <55155047+TheStormex@users.noreply.github.com>
2022-04-18 19:13:17 -0400

    lootbox number glitch fixed

--- ENG255-Lootbox/js/script.js
+++ ENG255-Lootbox/js/script.js
@@ -2,12 +2,8 @@
 
 /*****************
 
-Raving Redactionist
-Pippin Barr
-
-You are redacting a document, but it keeps becoming unredacted!
-Click the secret information to hide it, don't let all the
-secrets become revealed!
+The Lootbox Mechanic
+Che Tan
 
 ******************/
 let gameScreen;
@@ -868,7 +864,7 @@ function openLootbox() {
           thisItemName = "Food";
           break;
         case 3: // lootbox
-          lootboxCount == thisItemQuantity;
+          lootboxCount += thisItemQuantity;
           thisItemName = "Lootbox";
           break;
         default:
